bd4c8093f83f9af387d4804456bf66566b370cba
SimonAllenOliver <simonallenoliver@gmail.com>
2024-01-08 19:44:19 -0600

    for loops

---
+++ for_loops_basic1.py
@@ -0,0 +1,9 @@
+# 1 basic - print all integers from 0 to 150
+
+# for i in range(151):
+#     print(i)
+
+# 2 multiples of 5 - Print all the multiples of 5 from 5 to 1,000
+
+for i in range(5,1001,5):
+    print(i)
